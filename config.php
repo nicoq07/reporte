323de2a18afbf6fe3386b477a45f5fdc0250db17
nicoq07 <nicoq07@gmail.com>
2018-06-22 01:32:47 -0300

    Desarrollo

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// CONSTANTES
+define('LOGEO', __DIR__ . '/log/logeo.log');
+define('PATH_ARCHIVOS', __DIR__ . '/archivos/dat' . date('d-m-Y h:i:s') . '.txt');
+
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+?>
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 ?>
 <!DOCTYPE head PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="framework/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="webroot/css/bootstrap.css">
 		<script src="framework/js/bootstrap.js"></script> 
 	</head>
 	<body>
